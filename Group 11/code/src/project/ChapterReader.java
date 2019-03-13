f5fc070ba962cf48176726e3ca9ae77145d50e12
Mahmudul Hasan <mahmudul.hasan152@northsouth.edu>
2019-03-13 19:57:04 +0600

    refactor version 1

--- Group 11/code/src/project/ChapterReader.java
+++ Group 11/code/src/project/ChapterReader.java
@@ -1,4 +1,50 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class ChapterReader {
+	private File[] listOfFiles;
+	public ChapterReader(File[] x )
+	{
+		
+		this.
+	}
 
+	public ChapterWordCount[] GetChapterWiseWordFrequency() {
+		ArrayList<ChapterWordCount> listofchapterwords = new ArrayList<ChapterWordCount>();
+		for(int x = 0; x < this.listOfFiles.length; ++x) {
+			File file = this.listOfFiles[x];
+			
+			ChapterWordCount wordCount = new ChapterWordCount(this.getChapterName(file.getName()));
+			
+			Scanner s = new Scanner(file).useDelimiter(" +");
+			//System.out.println(listOfFiles[i].getName());
+			int poi= 0;
+			
+			while (s.hasNextLine())
+			{
+				
+				String mn = s.next();
+			    //h[i].putIfAbsent(mn,poi);
+			    wordCount.addCount(mn);
+			 }
+			listofchapterwords.add(wordCount);
+		}
+		
+		return listofchapterwords.toArray();
+	}
+	
+	private String getChapterName(String fileName) {
+		
+	}
 }
+
+ChapterWordCount[] listOfChapterWordCount = new ChapterReader(listOfFiles).GetChapterWiseWordFrequency();
+
+
+ChapterWordContainer container = new ChapterWordContainer(listOfChapterWordCount);
+QuestionPaperWords qpWords = new QuestionPaperWords(qpFile);
+String[] wordsInQp = qpWords.getAllWords();
+
+String chapter = container.FindChapter(wordsInQp);
+
+
---
+++ Group 11/code/src/project/ChapterWordCount.java
@@ -0,0 +1,50 @@
+import java.util*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class ChapterWordCount {
+	private String chapterName;
+	 private LinkedHashMap<String, Integer> h = new LinkedHashMap();
+	
+	public ChapterWordCount(String chapterName) {
+		this.chapterName = chapterName;
+	}
+	
+	public String getChapterName() {
+		return this.chapterName;
+	}
+	
+	public void addCount(String word)
+	{
+		
+		h.putIfAbsent(word,0);
+		 
+		 if((h.get(word))>=0)
+		 {
+			 int u = h.get(word);
+			 u++;
+			 h.put(word,u);
+		 }
+		
+	}
+	
+	public int getCount(String word) {
+		if (h.containsKey(word))
+		
+		return h.get(word);
+		else
+			return 0;
+		
+	}
+	
+	public string getWordWithMaxCount() {
+		
+	
+	}
+}
